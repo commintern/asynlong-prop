485282d238a75a64bc7614204f288636bcd52b5a
Dayu <commintern@hotmail.com>
2019-04-11 16:19:54 -0500

    se t the same random seed.

--- R/sim.R
+++ R/sim.R
@@ -31,6 +31,7 @@ simdataone <- function(nsample, p, infl, obscov_rate,lambda0_val,mu0,beta,alpha,
 # Main parallel computing
 simmain <- function(nrep,nsample, p, infl, obscov_rate,lambda0_val,mu0,beta,alpha,gamma,censor,horder) {
   #cat("Start: ")
+  set.seed(9102)
   simdatarep <- foreach(i = 1:nrep,.packages=c("asynlong","MASS","extraDistr","nleqslv"),.errorhandling = "remove") %do% {
     simdataone(nsample, p, infl, obscov_rate,lambda0_val,mu0,beta,alpha,gamma,censor)
   }
